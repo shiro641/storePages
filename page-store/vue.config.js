7572b48bf28d7cbf94d087ee71b93426b715c8e5
zhangjie.jie <zhangjie.jie@bytedance.com>
2022-12-18 16:01:39 +0800

    update

--- page-store/vue.config.js
+++ page-store/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // vue.config.js
+  publicPath: process.env.NODE_ENV === "production" ? "/storePages/" : "/",
 });
